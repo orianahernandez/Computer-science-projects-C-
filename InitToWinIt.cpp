0db7aaeadd38a76866dc699ce032153d50b3769e
orianahernandez <54334817+orianahernandez@users.noreply.github.com>
2024-02-06 16:49:04 -0500

    Create InitToWinIt.cpp

---
+++ InitToWinIt.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/*This program intends to show the entry information for a chosen competition,
+
+in this case "Air Guitar".  */
+
+int main() {
+
+	std::cout << "Air Guitar\n";
+
+	//Written by: Dennis Linde 
+
+	std::cout << "Burning Love " << std::endl;
+
+	std::cout << "As performed by: Elvis Presley\n";
+
+	std::cout << "Time: 2:50\n";
+
+
+
+	return 0;
+
+}
